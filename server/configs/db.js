94e5cc792b70489b84c3abb7fe6ac765b650a389
Itai-Arnon <itai.arnon1@gmail.com>
2024-07-08 22:40:02 +0300

    more work on editDept
    close to finish

--- server/configs/db.js
+++ server/configs/db.js
@@ -4,7 +4,7 @@ const connectDB = () => {
   // Connect to MongoDB database
   mongoose
     .connect('mongodb://127.0.0.1:27017/Project1')
-    .then(() => console.log('MongoDB Connected to Project1'))
+    .then(() => console.log('MongoDB Connected to Factory HR Project'))
     .catch((error) => console.log(error));
 };
 
